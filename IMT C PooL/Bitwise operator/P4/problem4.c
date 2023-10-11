92b7ae3a7df3dbd935f9ffe0fc0a055e3c723cdd
Engy Essam <62803136+En9yCS@users.noreply.github.com>
2023-10-11 10:02:33 -0700

    Add files via upload

---
+++ IMT C PooL/Bitwise operator/P4/problem4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int clearNthBit(int num, int n) {
+    int mask = ~(1 << n);
+    return num & mask;
+}
+
+int main() {
+    int num, n, result;
+    
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    printf("Enter the position of the bit to clear (0-indexed): ");
+    scanf("%d", &n);
+    
+    result = clearNthBit(num, n);
+    
+    printf("Number after clearing the %dth bit: %d\n", n, result);
+    
+    return 0;
+}
