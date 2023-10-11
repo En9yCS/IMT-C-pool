056d5cfd7d069e44fcd24f574f993fa6544bbfa0
Engy Essam <62803136+En9yCS@users.noreply.github.com>
2023-10-11 09:46:00 -0700

    Add files via upload

---
+++ IMT C PooL/Bitwise operator/P3/problem3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int setNthBit(int num, int n) {
+    int mask = 1 << n;
+    return num | mask;
+}
+
+int main() {
+    int num, n, result;
+    
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    printf("Enter the position of the bit to set (0-indexed): ");
+    scanf("%d", &n);
+    
+    result = setNthBit(num, n);
+    
+    printf("Number after setting the %dth bit: %d\n", n, result);
+    
+    return 0;
+}
